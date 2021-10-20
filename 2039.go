fdf6a8acfabe95900cdec71761964d2c6730302f
gbald <lfld287@gmail.com>
2021-10-20 15:18:41 +0800

    first commit

---
+++ 2039.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func networkBecomesIdle(edges [][]int, patience []int) int {
+	serverNum:=len(patience)
+	minPath:=make([]int,serverNum)
+	for index:=range minPath{
+		minPath[index]=0
+	}
+	//先计算每个数据服务器到主服务器的最短路径
+	//长度一样，广度优先就行
+
+}
---
+++ 453.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func minMoves(nums []int) int {
+	//min == max
+	var minIdx = 0
+	var maxIdx = 0
+
+	for idx := range nums {
+		if nums[idx] < nums[minIdx] {
+			minIdx = idx
+		}
+		if nums[idx] > nums[maxIdx] {
+			maxIdx = idx
+		}
+	}
+	//其实就是选一个最大的数减1，直到全减到和最小的一样
+	count:=0
+	for idx:=range nums{
+		if idx==minIdx{
+			continue
+		}
+		count+=nums[idx]-nums[minIdx]
+	}
+	return count
+}
